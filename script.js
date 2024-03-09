29c9f199b2d71ff71682973dfe2146cb90b3aeea
BehzadJanNesar <behzadjannesar.2001@gmail.com>
2024-03-09 11:32:10 +0330

    add neighbour country

--- script.js
+++ script.js
@@ -9,19 +9,22 @@ const countriesContainer = document.querySelector('.countries');
 
 // create countries Element
 function createCountryElement(mainCountryData, className = '') {
-  let { flag, name, region, population, languages, currencies } =
+  let { flags, name, region, population, languages, currencies } =
     mainCountryData;
+  let languageName, currencieName;
+  for (const i in languages) languageName = languages[i];
+  for (const i in currencies) currencieName = currencies[i].name;
   const html = `
     <article class="country ${className}">
-      <img class="country__img" src="${flag}" />
+      <img class="country__img" src="${flags.svg}" />
       <div class="country__data">
-        <h3 class="country__name">${name}</h3>
+        <h3 class="country__name">${name.common}</h3>
         <h4 class="country__region">${region}</h4>
         <p class="country__row"><span>👫</span>${(population / 1000000).toFixed(
           1
         )}</p>
-        <p class="country__row"><span>🗣️</span>${languages[0].name}</p>
-        <p class="country__row"><span>💰</span>${currencies[0].name}</p>
+        <p class="country__row"><span>🗣️</span>${languageName}</p>
+        <p class="country__row"><span>💰</span>${currencieName}</p>
       </div>
     </article>
   `;
@@ -31,35 +34,36 @@ function createCountryElement(mainCountryData, className = '') {
 // ///////////////////////////////////////////////////////////////////////////
 const countriesData = function (country) {
   const request = new XMLHttpRequest();
-  request.open('GET', `https://restcountries.com/v2/name/${country}`);
+  const routeURL = country === 'all' ? 'all' : `name/${country}`;
+  request.open('GET', `https://restcountries.com/v3.1/${routeURL}`);
   request.send();
 
   request.addEventListener('load', function () {
     const [data1] = JSON.parse(this.responseText);
     createCountryElement(data1);
 
-    const request2 = new XMLHttpRequest();
-    const neighber = data1.borders;
-    console.log(neighber[0]);
-    request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighber[0]}`);
-    request2.send();
+    const neighbour = data1.borders?.[0];
+    if (!neighbour) return;
 
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbour}`);
+    request2.send();
     request2.addEventListener('load', function () {
       const [data2] = JSON.parse(this.responseText);
-      console.log(data2);
-      createCountryElement(data2, 'neighber');
+      createCountryElement(data2, 'neighbour');
     });
   });
 };
 
 let changed;
 check.addEventListener('change', function () {
-  changed ? (changed = false) : (changed = true);
+  changed = this.checked;
 });
 
 input.focus();
 form.addEventListener('submit', e => {
   e.preventDefault();
+  countriesContainer.innerHTML = '';
   countriesData(input.value);
   input.value = '';
 });
